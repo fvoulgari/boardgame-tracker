d7b8dd8fc81e0318f3a0d7715a6a786c14f0d58c
fvoulgari <fayrevof@gmail.com>
2024-11-03 22:12:53 +0200

    latest

--- user-service/index.js
+++ user-service/index.js
@@ -1,3 +1,17 @@
+const appInsights = require('applicationinsights');
+
+const INSIGHT_KEY = process.env.INSIGHT_KEY;
+
+// Use either the instrumentation key or connection string
+appInsights.setup(INSIGHT_KEY)
+  .setAutoCollectRequests(true)    // Enable request tracking
+  .setAutoCollectDependencies(true) // Enable dependency tracking
+  .setAutoCollectExceptions(true)   // Enable error tracking
+  .setAutoCollectPerformance(true)  // Enable performance tracking
+  .setAutoCollectConsole(true, true) // Collect console.log and console.error
+  .start();                         // Starts the Application Insights SDK
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
